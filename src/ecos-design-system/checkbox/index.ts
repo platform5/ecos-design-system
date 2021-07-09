ad423390afe54d314867dc903ad06b2190b97b76
Ben Girardet <ben@platform5.ch>
2021-07-09 14:16:57 +0200

    checkbox: fix indicators

--- src/ecos-design-system/checkbox/index.ts
+++ src/ecos-design-system/checkbox/index.ts
@@ -1,5 +1,5 @@
 import { css } from '@microsoft/fast-element';
-import { checkboxTemplate as template, Checkbox, ElementDefinitionContext, FoundationElementDefinition } from '@microsoft/fast-foundation';
+import { checkboxTemplate as template, Checkbox, CheckboxOptions, ElementDefinitionContext, FoundationElementDefinition } from '@microsoft/fast-foundation';
 import { checkboxStyles as styles } from '@microsoft/fast-components';
 
 const maxRadiusCheckboxStyles = css`
@@ -15,8 +15,26 @@ const overrideStyles = (context: ElementDefinitionContext, definition: Foundatio
   `
 }
 
-export const ecosCheckbox = Checkbox.compose({
+export const ecosCheckbox = Checkbox.compose<CheckboxOptions>({
   baseName: 'checkbox',
   template,
-  styles: overrideStyles
+  styles: overrideStyles,
+  checkedIndicator: `
+      <svg
+          aria-hidden="true"
+          part="checked-indicator"
+          class="checked-indicator"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+      >
+          <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M8.143 12.6697L15.235 4.5L16.8 5.90363L8.23812 15.7667L3.80005 11.2556L5.27591 9.7555L8.143 12.6697Z"
+          />
+      </svg>
+  `,
+  indeterminateIndicator: `
+      <div part="indeterminate-indicator" class="indeterminate-indicator"></div>
+  `,
 });
