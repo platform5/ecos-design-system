a7165ed184dd0b721184a70ea202ff740235b10c
Ben Girardet <ben@platform5.ch>
2021-07-14 08:07:13 +0200

    Table: separated and bordered attrs

--- src/ecos-design-system/table/styles.ts
+++ src/ecos-design-system/table/styles.ts
@@ -1,3 +1,4 @@
+import { neutralLayerFloating, neutralLayer3, neutralFillActive, neutralStrokeRest } from '@microsoft/fast-components';
 import { css } from '@microsoft/fast-element';
 import { display } from "@microsoft/fast-foundation";
 
@@ -10,5 +11,58 @@ export const TableStyles = css`
 
   ::slotted(ecos-table-row) {
     padding: 16px 0;
+  }
+
+  ::slotted(ecos-table-row.active) {
+    padding: 16px 0;
+    background: ${neutralFillActive};
+  }
+
+  :host([hoverable]) ::slotted(ecos-table-row){
+    padding-left: var(--responsive-spacing-unit-lg);
+    padding-right: var(--responsive-spacing-unit-lg);
+  }
+
+  :host([separated]) {
+    background: transparent;
+    border: none;
+    box-shadow: none;
+  }
+  
+  :host([separated]) ::slotted(ecos-table-row) {
+    border-radius: calc(var(--control-corner-radius) * 1px);
+    background-color: ${neutralLayerFloating};
+    margin-left: 0;
+    margin-right: 0;
+    margin-top: 8px;
+    padding-left: var(--responsive-spacing-unit-lg);
+    padding-right: var(--responsive-spacing-unit-lg);
+  }
+  :host([separated][hoverable]) ::slotted(ecos-table-row:hover) {
+    border-radius: calc(var(--control-corner-radius) * 1px);
+    background: ${neutralLayer3};
+  }
+  :host([separated]) ::slotted(ecos-table-row:first-child) {
+    margin-top: 0px;
+  }
+
+  :host([bordered]) {
+    background: transparent;
+    border: none;
+    box-shadow: none;
+  }
+  
+  :host([bordered]) ::slotted(ecos-table-row) {
+    border-radius: 0;
+    margin-left: 0;
+    margin-right: 0;
+    min-height: calc(var(--spacing-unit-xl) * 1.5);
+    border-top: 1px solid ${neutralStrokeRest};
+  }
+  :host([bordered][hoverable]) ::slotted(ecos-table-row:hover) {
+    background: ${neutralLayer3}
+  }
+  :host([bordered]) ::slotted(ecos-table-row:first-child) {
+    border-top: none;
   }
 `;
