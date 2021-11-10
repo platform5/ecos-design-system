c2aad0eb67be34c02a84dde8f8ae0fe73cfda5af
Ben Girardet <ben@platform5.ch>
2021-11-10 10:54:16 +0100

    card: fix auto/darken/lighten fill-color

--- src/ecos-design-system/card/index.ts
+++ src/ecos-design-system/card/index.ts
@@ -1,7 +1,7 @@
 import { cardStyles as styles, fillColor, isDark, SwatchRGB, Card } from '@microsoft/fast-components';
-import { attr, css } from '@microsoft/fast-element';
+import { attr, css, observable } from '@microsoft/fast-element';
 import { parseColorHexRGB } from '@microsoft/fast-colors';
-import { cardTemplate as template, ElementDefinitionContext, FoundationElementDefinition } from '@microsoft/fast-foundation';
+import { cardTemplate as template, DesignTokenSubscriber, ElementDefinitionContext, FoundationElementDefinition } from '@microsoft/fast-foundation';
 
 import { lightenViaLAB, darkenViaLAB, ColorRGBA64 } from "@microsoft/fast-colors";
 
@@ -32,53 +32,71 @@ export class EcosCard extends Card {
   @attr({attribute: 'fill-color'})
   fillColor: 'lighten' | 'darken' | 'auto' | 'unset' | string = 'auto';
 
+  @observable
+  private effectiveFillColor: 'unset' | string = 'unset';
+
+  private effectiveParentElement: HTMLElement;
+  private parentElementFillColorHanlder: DesignTokenSubscriber<typeof fillColor> = {
+    handleChange: () => {
+      this.determineEffectiveFillColor();
+    }
+  }
+
+  private determineEffectiveFillColor(): void {
+    if (!this.effectiveParentElement) {
+      return;
+    }
+    const parentFillColor = fillColor.getValueFor(this.effectiveParentElement) as SwatchRGB;
+
+    let effectiveAction = 'auto';
+    if (this.fillColor === 'auto') {
+      if (isDark(parentFillColor)) {
+        effectiveAction = 'lighten';
+      } else if(parentFillColor.r === 1 && parentFillColor.g === 1 && parentFillColor.b === 1) {
+        // isWhite
+        effectiveAction = 'darken';
+      } else {
+        effectiveAction = 'lighten';
+      }
+    } else if (this.fillColor === 'darken' || this.fillColor === 'lighten') {
+      effectiveAction = this.fillColor;
+    }
+
+    const fillcolor = new ColorRGBA64(parentFillColor.r, parentFillColor.g, parentFillColor.b);
+    const newFill = effectiveAction === 'lighten' ? lightenViaLAB(fillcolor, 0.2) : darkenViaLAB(fillcolor, 0.2);
+    const swatch = SwatchRGB.create(newFill.r, newFill.g, newFill.b);
+    this.effectiveFillColor = swatch.toColorString();
+  }
+
   public fillColorChanged(): void {
     if (!this.$fastController.isConnected) {
       return;
     }
-    if (this.fillColor === 'unset') {
-      this.unsetFillcolor();
-    } else if (['lighten', 'darken', 'auto'].includes(this.fillColor)) {
-      this.autoFillColor(this.fillColor as 'lighten' | 'darken' | 'auto');
-    } else {
-      this.setFillColor(this.fillColor);
-    }
-    try {
-      const color = parseColorHexRGB(this.fillColor);
-      if (color) {
-        const swatch = SwatchRGB.create(color.r, color.g, color.b);
-        fillColor.setValueFor(this, swatch);
+
+    if (this.fillColor === 'auto' || this.fillColor === 'lighten' || this.fillColor === 'darken') {
+      this.effectiveParentElement = this.parentElement || this.ownerDocument.body;
+      if (this.effectiveParentElement) {
+        fillColor.subscribe(this.parentElementFillColorHanlder, this.effectiveParentElement);
       }
-    } catch (error) {
-      console.warn('Failed to process fillColorChanged', this.fillColor)
+      this.determineEffectiveFillColor();
+    } else {
+      if (this.effectiveParentElement) {
+        fillColor.unsubscribe(this.parentElementFillColorHanlder, this.effectiveParentElement)
+      }
+      this.effectiveFillColor = this.fillColor;
     }
+
   }
-
-  private autoFillColor(action: 'lighten' | 'darken' | 'auto'): void {
-
-    try {
-      fillColor.setValueFor(this, (element) => {
-        const parentFillColor = fillColor.getValueFor(this.parentElement || this.ownerDocument.body) as SwatchRGB;
-        let effectiveAction = action;
-        if (action === 'auto') {
-          if (isDark(parentFillColor)) {
-            effectiveAction = 'lighten';
-          } else if(parentFillColor.r === 1 && parentFillColor.g === 1 && parentFillColor.b === 1) {
-            // isWhite
-            effectiveAction = 'darken';
-          } else {
-            effectiveAction = 'lighten';
-          }
-        }
-
-        const fill = fillColor.getValueFor(element.parentElement || element.ownerDocument.body) as SwatchRGB;
-        const fillcolor = new ColorRGBA64(fill.r, fill.g, fill.b);
-        const newFill = effectiveAction === 'lighten' ? lightenViaLAB(fillcolor, 0.2) : darkenViaLAB(fillcolor, 0.2);
-        const swatch = SwatchRGB.create(newFill.r, newFill.g, newFill.b);
-        return swatch;
-      });
-    } catch (error) {
-      console.warn('Failed to process fillColor design token', this.fillColor);
+  
+  public effectiveFillColorChanged(): void {
+    if (!this.$fastController.isConnected) {
+      return;
+    }
+    if (this.effectiveFillColor === 'unset') {
+      this.unsetFillcolor();
+    } else {
+      // here this.fillColor is a string of color
+      this.setFillColor(this.effectiveFillColor);
     }
   }
 
